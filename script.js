d89182294061c99c42176ef40798e4b8d8b623b4
Joel Lopez <jolopez.satx@gmail.com>
2023-09-23 11:59:40 -0500

    origin main

--- script.js
+++ script.js
@@ -12,9 +12,12 @@ $(function () {
   // useful when saving the description in local storage?
 
   saveButton.on('click', function(){
+//'this' is refering to location and maipulation of elements relative to the cloick button.
+    var timeBlock = $(this).parent();
+    //got a handle on the id attr
+    var timeBlockID = timeBlock.attr('id'); 
 
-
-
+    localStorage.setItem(timeBlock, timeBlockID);
   });
   //
   // TODO: Add code to apply the past, present, or future class to each time
@@ -22,6 +25,13 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  //dayjs current hour:
+
+  var cHour = dayjs().hour() // gets current hour
+
+
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
